1b5b43d628157b534cb8b2ef0e109ba874991177
Kael <khang.tran@mozox.com>
2025-02-18 11:33:20 +0700

    clean up unused files

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE sensor_data (
-                             id SERIAL PRIMARY KEY,
-                             sensor_id VARCHAR(50),
-                             temperature DOUBLE PRECISION,
-                             humidity DOUBLE PRECISION,
-                             timestamp TIMESTAMP,
-                             is_anomaly BOOLEAN
-);
